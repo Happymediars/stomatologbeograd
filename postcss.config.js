0ace8f65a766457a86575f3fc2db3d0329b8e299
Happymediars <marko@happymedia.rs>
2026-03-04 05:07:35 -0800

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/**
+ * PostCSS configuration for the project.  This file defines the
+ * plugins used to process CSS.  Tailwind CSS and Autoprefixer are
+ * configured to generate utility classes and vendor prefixes.
+ */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
